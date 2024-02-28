8e07645e4218d764d05abd79bfd5ead3c064949f
Marcela <marcela.mrf17@hotmail.com>
2024-02-28 20:37:35 -0300

    atualizamos o pom.xml inserimos o primeiro caso de teste

---
+++ src/test/java/projet/rest/test/MyTest.java
@@ -0,0 +1,22 @@
+package projet.rest.test;
+
+import static org.junit.Assert.assertEquals;
+
+
+import org.junit.jupiter.api.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+public class MyTest {
+	
+	Response response;
+	
+	@Test
+	public void validarLista() {
+		response = RestAssured.get("https://reqres.in/api/users?page=2");
+		assertEquals(200, response.statusCode());
+		
+	}
+
+}
